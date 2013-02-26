7e9d6c2b571541b5ca8deb360f4aea5cb1f0c11c
ITD27M01 <igortiunov@gmail.com>
2013-02-26 16:10:20 +0400

    CHANGE

--- backup/bitrix.sh
+++ backup/bitrix.sh
@@ -1,54 +1,54 @@
 #!/bin/sh
-echo " " > /root/wwwbackup.log
-echo " " >> /root/wwwbackup.log
-echo "################################  BEGIN  ###################################" >> /root/wwwbackup.log
-date >> /root/wwwbackup.log
-echo "1. Create bitrix database dump." >> /root/wwwbackup.log
-rm -rvRf /home/bitrix/www/bitrix/backup/*.gz >> /root/wwwbackup.log 2>&1
-rm -rvRf /home/bitrix/www/bitrix/backup/*.sql >> /root/wwwbackup.log 2>&1
+echo " "
+echo " "
+echo "################################  BEGIN  ###################################"
+date
+echo "1. Create bitrix database dump."
+rm -rvRf /home/bitrix/www/bitrix/backup/*.gz 
+rm -rvRf /home/bitrix/www/bitrix/backup/*.sql
 filedump=`eval date +%Y.%m.%d`".sql"
 filebackup=`eval date +%Y.%m.%d`".tar.gz"
 backupfolder=backup
 #backupfolder=`eval date +%d.%m.%Y`
-for list in `mysql -e "use bitrix; show tables;" | sed '/^Tables_in/d'` ; do
+for list in `mysql -e "use bitrix; show tables;" | tail -n +2` ; do
     mysqldump  bitrix $list | sed '/^\/\*/d' >> /home/bitrix/www/bitrix/backup/$filedump
 done
-echo " DUMP COMPLETE." >> /root/wwwbackup.log
-echo "2. Mount share directory for backup." >> /root/wwwbackup.log
+echo " DUMP COMPLETE." 
+echo "2. Mount share directory for backup."
 var1=`cat /etc/mtab | grep "/mnt"`
 if [ -n "$var1" ]
  then
-  echo "  /mnt already mounted as:" >> /root/wwwbackup.log
-  echo $var1 >> /root/wwwbackup.log
+  echo "  /mnt already mounted as:"
+  echo $var1
   while [ -n "$var1" ]; do
-   umount -fvl /mnt >> /root/wwwbackup.log 2>&1
+   umount -fvl /mnt
    var1=`cat /etc/mtab | grep "/mnt"`
   done
 fi
-rm -rvRf /mnt/* >> /root/wwwbackup.log 2>&1
-mount -t cifs //192.168.1.1/backup /mnt --verbose -o username=backup@domain.local,password=***,rw >> /root/wwwbackup.log 2>&1
-echo " folder contains:" >> /root/wwwbackup.log
-ls -la /mnt/www >> /root/wwwbackup.log 2>&1
+rm -rvRf /mnt/*
+mount -t cifs //192.168.1.1/backup /mnt --verbose -o username=backup@domain.local,password=***,rw
+echo " folder contains:"
+ls -la /mnt/www
 if [ -d /mnt/www ]
  then
-  echo "2. Backup site." >> /root/wwwbackup.log
+  echo "2. Backup site."
   cd /home/bitrix/www
     if [ -d /mnt/www/backup ]
 	then
-	    echo "Folder for backup exist." >> /root/wwwbackup.log
+	    echo "Folder for backup exist."
 	else
-	    echo "Folder for backup not exist, create" >> /root/wwwbackup.log
-	    mkdir /mnt/www/$backupfolder >> /root/wwwbackup.log 2>$1
+	    echo "Folder for backup not exist, create"
+	    mkdir /mnt/www/$backupfolder
     fi
-  tar -cvpzf /mnt/www/$backupfolder/$filebackup * .??* >> /root/wwwbackup.log 2>&1
-  echo " folder contains:" >> /root/wwwbackup.log 2>&1
-  ls /mnt/www >> /root/wwwbackup.log
-  umount -fvl /mnt >>/root/wwwbackup.log 2>&1
-  rm -rvRf /mnt/* >>/root/wwwbackup.log 2>&1
-  echo " Backup complete." >> /root/wwwbackup.log
+  tar -cvpzf /mnt/www/$backupfolder/$filebackup * .??*
+  echo " folder contains:"
+  ls /mnt/www
+  umount -fvl /mnt
+  rm -rvRf /mnt/*
+  echo " Backup complete."
  else
-  echo "2. Smbmount ERROR." >>/root/wwwbackup.log
-  umount -fvl /mnt >>/root/wwwbackup.log 2>&1
-  rm -rvRf /mnt/* >>/root/wwwbackup.log 2>&1
+  echo "2. Smbmount ERROR."
+  umount -fvl /mnt
+  rm -rvRf /mnt/*
 fi
-echo "#################################  END  ####################################" >> /root/wwwbackup.log
+echo "#################################  END  ####################################"
