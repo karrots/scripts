5094c0b5b3ea7844195305ef3f33d9b7a30fffbf
ITD27M01 <igortiunov@gmail.com>
2014-01-24 12:47:56 +0400

    Some changes

--- nagios/check_eigrp.c
+++ nagios/check_eigrp.c
@@ -176,7 +176,7 @@ int main(int argc, char *argv[])
 	} else {
 //Create SNMP session
 		void* session = snmpopen(globalArgs.COMMUNITY, globalArgs.HOSTNAME, globalArgs.timeOut);
-//Create buffer for snmp OID from concatenate EIGRP AS number:
+//Create buffer for snmp OID
 		char snmpOID[100];
 		memset(snmpOID, 0, 100);
 //Create buffer for SNMP output value (peercount). ~4G is a maximum namber + \0
